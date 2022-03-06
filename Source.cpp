5dfde8e65c234e81a8b44860c7cd10be4bd37ae2
dneves02 <71541305+dneves02@users.noreply.github.com>
2022-03-05 19:03:06 -0800

    SI3 3D-version

--- Source.cpp
+++ Source.cpp
@@ -15,17 +15,17 @@ using namespace std;
 #define BOARDDOT '.'
 #define BOARDSIZE 3
 
-bool read(char board[][BOARDSIZE], const char* fileName);
-bool write(const char board[][BOARDSIZE], const char* fileName);
-void display(const char board[][BOARDSIZE]);
-bool didWin(const char board[][BOARDSIZE], char turn);
+bool read(char board[][][BOARDSIZE], const char* fileName);
+bool write(const char board[][][BOARDSIZE], const char* fileName);
+void display(const char board[][][BOARDSIZE]);
+bool didWin(const char board[][][BOARDSIZE], char turn);
 
 /**********************************************************************
  * Keeps the data and calles the read/display/write functions
  ***********************************************************************/
 int main()
 {
-    char board[BOARDSIZE][BOARDSIZE];
+    char board[BOARDSIZE][BOARDSIZE][BOARDSIZE];
 
     // read the board
     char fileName[256];
@@ -56,7 +56,7 @@ int main()
  * READ
  * Read the board from the specified filename
  *************************************************************/
-bool read(char board[][BOARDSIZE], const char* fileName)
+bool read(char board[][][BOARDSIZE], const char* fileName)
 {
     assert(*fileName);
 
@@ -68,13 +68,14 @@ bool read(char board[][BOARDSIZE], const char* fileName)
     // read 9 symbols, hopefully they are . X O
     for (int r = 0; r < BOARDSIZE; r++)
         for (int c = 0; c < BOARDSIZE; c++)
-        {
-            fin >> board[r][c];
-            assert(!fin.fail());
-            assert(board[r][c] == BOARDX ||
-                board[r][c] == BOARDO ||
-                board[r][c] == BOARDDOT);
-        }
+            for(int z = 0; z < BOARDSIZE; z++)
+            {
+                fin >> board[r][c][z];
+                assert(!fin.fail());
+                assert(board[r][c][z] == BOARDX ||
+                    board[r][c][z] == BOARDO ||
+                    board[r][c][z] == BOARDDOT);
+            }
 
     // close the file
     fin.close();
@@ -85,7 +86,7 @@ bool read(char board[][BOARDSIZE], const char* fileName)
  * WRITE
  * Write to fileName the board data
  *********************************************************/
-bool write(const char board[][BOARDSIZE], const char* fileName)
+bool write(const char board[][][BOARDSIZE], const char* fileName)
 {
     assert(fileName[0] != '\0');
 
@@ -94,10 +95,11 @@ bool write(const char board[][BOARDSIZE], const char* fileName)
     if (fout.fail())
         return false;
 
-    // write my 9 symbols
+    // write my 9 symbols 3 times in 3 levels
     for (int r = 0; r < BOARDSIZE; r++)
         for (int c = 0; c < BOARDSIZE; c++)
-            fout << board[r][c] << (c == 2 ? '\n' : ' ');
+            for(int z = 0; z < BOARDSIZE; z++)
+                fout << board[r][c][z] << (c == 2 ? '\n' : ' ') << (z == 2 ? '\n' : ' ');
 
     // close it!
     fout.close();
@@ -109,7 +111,7 @@ bool write(const char board[][BOARDSIZE], const char* fileName)
  * DISPLAY
  * Display the contents the the screen
  *****************************************************/
-void display(const char board[][BOARDSIZE])
+void display(const char board[][][BOARDSIZE])
 {
     // loop through each row
     for (int r = 0; r < BOARDSIZE; r++)
@@ -121,14 +123,19 @@ void display(const char board[][BOARDSIZE])
         // now, on each row, do the column stuff
         for (int c = 0; c < BOARDSIZE; c++)
         {
-            // display a space for the dot
-            if (board[r][c] == BOARDDOT)
-                cout << "   ";
-            else
-                cout << " " << board[r][c] << " ";
 
-            // end with a | or a newline
-            cout << (c == 2 ? '\n' : '|');
+            for (int z = 0; c < BOARDSIZE; z++)
+            {
+                // display a space for the dot
+                if (board[r][c][z] == BOARDDOT)
+                    cout << "   ";
+                else
+                    cout << " " << board[r][c][z] << " ";
+
+                // end with a | or a newline
+                cout << (c == 2 ? '\n' : '|');
+                cout << (z == 2 ? '\n' : '|');
+            }
         }
     }
 
@@ -146,32 +153,35 @@ void display(const char board[][BOARDSIZE])
  * Did a given player (determined by the "turn"
  * variable) win the game?
  *******************************************/
-bool didWin(const char board[][BOARDSIZE], char turn)
+bool didWin(const char board[][][BOARDSIZE], char turn)
 {
 
     for (int i = 0; i < BOARDSIZE; i++)
     {
         for (int j = 0; j < BOARDSIZE; j++)
         {
-            if ((board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]) &&
-                (turn == BOARDX))
-                return false;
-            else if ((board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j]) &&
-                (turn == BOARDX))
-                return false;
-            else if (((board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2]) ||
-                (board[i + 2][j] == board[i + 1][j + 1] && board[i][j + 2])) && turn == BOARDX)
-                return false;
+            for(int z = 0; z < BOARDSIZE; z++)
+            {
+                if ((board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]) &&
+                    (turn == BOARDX))
+                    return false;
+                else if ((board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j]) &&
+                    (turn == BOARDX))
+                    return false;
+                else if (((board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2]) ||
+                    (board[i + 2][j] == board[i + 1][j + 1] && board[i][j + 2])) && turn == BOARDX)
+                    return false;
 
-            else if ((board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]) &&
-                (turn == BOARDO))
-                return true;
-            else if ((board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j]) &&
-                (turn == BOARDO))
-                return true;
-            else if (((board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2]) ||
-                (board[i + 2][j] == board[i + 1][j + 1] && board[i][j + 2])) && turn == BOARDO)
-                return true;
+                else if ((board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]) &&
+                    (turn == BOARDO))
+                    return true;
+                else if ((board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j]) &&
+                    (turn == BOARDO))
+                    return true;
+                else if (((board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2]) ||
+                    (board[i + 2][j] == board[i + 1][j + 1] && board[i][j + 2])) && turn == BOARDO)
+                    return true;
+            }
         }
     }
 }
